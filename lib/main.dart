06fffa36dd771a8c3ca96fa261c838809f56c6c2
a666hn <adidot78@gmail.com>
2021-12-17 01:09:13 +0700

    Add stateful widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,28 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const DiceeRollingApp());
+  runApp(const DiceeRollingApp(
+    title: 'DICEE ROLLING APP',
+  ));
 }
 
 class DiceeRollingApp extends StatelessWidget {
-  const DiceeRollingApp({Key? key}) : super(key: key);
+  const DiceeRollingApp({Key? key, this.title = ''}) : super(key: key);
+
+  final String title;
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.red.shade400,
+        backgroundColor: Colors.red,
         appBar: AppBar(
-          backgroundColor: Colors.red.shade400,
-          title: const Text(
-            'DICEE ROLLING APP',
-            style: TextStyle(
+          backgroundColor: Colors.red,
+          centerTitle: true,
+          title: Text(
+            title,
+            style: const TextStyle(
               fontFamily: 'Source Sans Pro',
               fontWeight: FontWeight.w800,
             ),
@@ -28,11 +34,66 @@ class DiceeRollingApp extends StatelessWidget {
   }
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
   const DicePage({Key? key}) : super(key: key);
 
   @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDiceNumber = 1;
+  int rightDiceNumber = 1;
+
+  @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextButton(
+                onPressed: () {
+                  setState(() {
+                    if (leftDiceNumber == 6) {
+                      leftDiceNumber = 1;
+                    } else {
+                      leftDiceNumber++;
+                    }
+                  });
+
+                  if (kDebugMode) {
+                    print(leftDiceNumber);
+                  }
+                },
+                child: Image.asset('assets/images/dice$leftDiceNumber.png'),
+              ),
+            ),
+          ),
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextButton(
+                onPressed: () {
+                  setState(() {
+                    if (rightDiceNumber == 6) {
+                      rightDiceNumber = 1;
+                    } else {
+                      rightDiceNumber++;
+                    }
+                  });
+
+                  if (kDebugMode) {
+                    print(rightDiceNumber);
+                  }
+                },
+                child: Image.asset('assets/images/dice$rightDiceNumber.png'),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
